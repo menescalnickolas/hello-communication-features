5ecd558cf20a56078c736a4366fe1a149af81aea
menescalnickolas <menescalnickolas@gmail.com>
2024-11-24 18:13:08 +0100

    Created test project

--- App.js
+++ App.js
@@ -1,11 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
 
-export default function App() {
+import * as Location from 'expo-location';
+import MapView from 'react-native-maps';
+
+import * as ImagePicker from 'expo-image-picker';
+const App = () => {
+  const [image, setImage] = useState(null);
+  const [location, setLocation] = useState(null);
+
+  const pickImage = async () => {
+    let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchImageLibraryAsync();
+
+      if (!result.canceled) setImage(result.assets[0]);
+      else setImage(null)
+    }
+  }
+
+  const takePhoto = async () => {
+    let permissions = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchCameraAsync();
+
+      if (!result.canceled) setImage(result.assets[0]);
+      else setImage(null)
+    }
+  }
+
+  const getLocation = async () => {
+    let permissions = await Location.requestForegroundPermissionsAsync();
+
+    if (permissions?.granted) {
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    } else {
+      Alert.alert("Permissions to read location aren't granted");
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title='Pick an image from the library'
+        onPress={pickImage}
+      />
+      <Button
+        title='Take a photo'
+        onPress={takePhoto}
+      />
+      {image &&
+        <Image source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />
+      }
+
+      <Button
+        title="Get my location"
+        onPress={getLocation}
+      />
+      {location &&
+        <MapView
+          style={{ width: 300, height: 200 }}
+          region={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />}
     </View>
   );
 }
@@ -13,8 +78,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+
+export default App;
